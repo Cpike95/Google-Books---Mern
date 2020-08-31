99e1ca809bb9e40408bb0f44e18b8b0587ad408c
Curtis Pike <cpike46@gmail.com>
2020-08-30 18:45:15 -0700

    removed api

--- routes/api/googleBooks.js
+++ routes/api/googleBooks.js
@@ -1,14 +1,13 @@
-// Export an object containing methods we'll use for accessing the GitHub Jobs API
+// // Export an object containing methods we'll use for accessing the GitHub Jobs API
 const router = require("express").Router();
-import axios from "axios";
+const searchController = require("../../controllers/searchController");
 
-// const booksController = require("../../controllers/booksController");
 
-// Matches with "/api/search"
-router.get("/search", query => {
-    return axios.get(
-      "https://www.googleapis.com/books/v1/volumes?q=" + {query} + "&key=AIzaSyDs5bl_uhk0NxueT4cv6AGTSW_3c6EeG2M"   
-    );
-  });
+// // const booksController = require("../../controllers/booksController");
+
+// // Matches with "/api/search"
+router.use("/search")
+    .get(searchController)
+
 
 module.exports = router;
